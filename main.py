20df1dbc5d1c6c8b3d829cff801d489c862a68e5
NestorIvanovich <115668331+NestorIvanovich@users.noreply.github.com>
2023-03-30 03:40:13 +0700

    скрип парсинга веб страниц

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 
 
+
 def get_source_html(url):
     driver = webdriver.Chrome(
         executable_path='chromedriver'
--- main2.py
+++ main2.py
@@ -3,33 +3,44 @@ from bs4 import BeautifulSoup
 from time import sleep
 
 headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)'
-                         ' AppleWebKit/537.36 (KHTML, like Gecko)'
+                         ' AppleWebKit/537.36 (HTML, like Gecko)'
                          ' Chrome/111.0.0.0 Safari/537.36'}
 
 list_card_url = []
 
-for count in range(1, 8):
-    sleep(3)
-    url = f'https://scrapingclub.com/exercise/list_basic/?page={count}'
 
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.text, 'lxml')
+def download(url):
+    resp = requests.get(url, stream=True)
+    r = open('/Users/alex/PycharmProjects/pythonParser/img/' + url.split("/")[-1], 'wb')
+    for value in resp.iter_content(1024*1024):
+        r.write(value)
+    r.close()
 
-    data = soup.find_all('div', class_='col-lg-4 col-md-6 mb-4')
-    for i in data:
-        card_url = 'https://scrapingclub.com' + i.find('a').get('href')
-        list_card_url.append(card_url )
-        print(card_url)
-for card_url in list_card_url:
-    response = requests.get(card_url, headers=headers)
-    soup = BeautifulSoup(response.text, 'lxml')
 
-    data = soup.find('div', class_='card mt-4 my-4')
-    name = data.find('h3', class_='card-title').text
-    price = data.find('h4').text
-    text = data.find('p', class_='card-text').text
-    url_img = 'https://scrapingclub.com' + \
-              data.find('img', class_='card-img-top img-fluid').get('src')
-wtf
+def get_url():
+    for count in range(1, 8):
+        url = f'https://scrapingclub.com/exercise/list_basic/?page={count}'
 
-    print(name, price, text, sep='\n')
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.text, 'lxml')
+
+        data = soup.find_all('div', class_='col-lg-4 col-md-6 mb-4')
+        for i in data:
+            card_url = 'https://scrapingclub.com' + i.find('a').get('href')
+            yield card_url
+
+
+def array_gen():
+    for card_url in get_url():
+        response = requests.get(card_url, headers=headers)
+        sleep(1)
+        soup = BeautifulSoup(response.text, 'lxml')
+
+        data = soup.find('div', class_='card mt-4 my-4')
+        name = data.find('h3', class_='card-title').text
+        price = data.find('h4').text
+        text = data.find('p', class_='card-text').text
+        url_img = 'https://scrapingclub.com' + \
+                  data.find('img', class_='card-img-top img-fluid').get('src')
+        download(url_img)
+        yield name, price, text, url_img
